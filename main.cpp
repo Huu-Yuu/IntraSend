0f54f7233e30647ed43d1bcc53943df871a9d29e
胡雨 <huyu98@vip.qq.com>
2026-02-14 16:34:46 +0800

    初始化提交，调整目录结构

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
-#include "mainwindow.h"
-
+#include "widget.h"
+#include "appinit.h"
 #include <QApplication>
-#include "QtWidgets/QApplication"
+#include <QFile>
+#include <QFont>
 int main(int argc, char *argv[])
 {
-    QGuiApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
     QApplication a(argc, argv);
-    MainWindow w;
+
+    //加载样式表
+    QFile file(":/css/index.css");
+    if (file.open(QFile::ReadOnly)) {
+        QString qss = QLatin1String(file.readAll());
+        qApp->setStyleSheet(qss);
+        file.close();
+    }
+
+    //全局字体
+    QFont font("Arial", 10);
+    a.setFont(font);
+
+    //屏幕拖动
+    AppInit::Instance()->start();
+
+    Widget w;
     w.show();
+
     return a.exec();
 }
